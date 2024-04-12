a6400ce86c050939439cfc85febdf6042c884f8a
juanmloaiza <juanm.loaiza@udea.edu.co>
2024-04-12 10:47:31 -0500

    Modificación a 2d

--- quiz.py
+++ quiz.py
@@ -10,3 +10,7 @@ matriz_3d= matriz_4d.copy()[:,:,:,0:1]
 print("Dimensiones:" , matriz_3d.shape)
 print("Tamaño:" , matriz_3d.size)
 print("Tipo de datos:" , matriz_3d.type)
+
+#Modificación y pasar a 2d
+matriz_2d= matriz_3d.reshape(-1,matriz_3d.shape[-1])
+
